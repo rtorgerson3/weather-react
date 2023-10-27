7ca20f966c82d5865142b77b48897d28bf8f394c
rtorgerson3 <107807540+rtorgerson3@users.noreply.github.com>
2023-10-27 11:56:28 -0700

    deleted files

--- src/CityName.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export default function CityName() {
-  let weatherData = {
-    city: "Sydney",
-  };
-  return <h2>{weatherData.city}</h2>;
-}
--- src/CityTemperature.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export default function CityTemperature() {
-  let weatherData = {
-    citytemperature: 75,
-  };
-  return <span class="current-degrees">{weatherData.citytemperature}°</span>;
-}
