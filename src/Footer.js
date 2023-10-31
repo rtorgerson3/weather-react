e3c820eaabd5ec8e970b2f40f382dd1628647068
rtorgerson3 <107807540+rtorgerson3@users.noreply.github.com>
2023-10-31 14:33:16 -0700

    Added rel="noopener noreferrer" to target=_blank

--- src/Footer.js
+++ src/Footer.js
@@ -5,11 +5,22 @@ export default function Footer() {
     <div>
       {" "}
       This project was coded by Rachel Torgerson and is open-sourced on{" "}
-      <a href="https://github.com/rtorgerson3/weather-react" target="blank">
+      <a
+        href="https://github.com/rtorgerson3/weather-react"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         Github
       </a>{" "}
       and hosted on{" "}
-      <a href="https://ornate-douhua-4f476e.netlify.app/"> Netlify </a>
+      <a
+        href="https://ornate-douhua-4f476e.netlify.app/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {" "}
+        Netlify{" "}
+      </a>
     </div>
   );
 }
